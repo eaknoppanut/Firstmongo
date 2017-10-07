58ff53b11e4f2f8ce57673fbecd478557508746f
eaknoppanut <eaknoppanut@hotmail.com>
2017-10-07 14:23:36 +0700

    lab restfull

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,8 +1,18 @@
-// create new file ./app/controllers/index.controller.js
+var path =require("path")
+
 exports.index = (req, res) => {
-    res.send('Hello World');
+       res.sendFile((path.join(__dirname + '/../views/index.html')));
+}
+exports.help = (req, res) => {
+npmres.send('Help me');
 }
 
-exports.help = (req, res) => {
-    res.send('Help me');
+
+exports.home = (req, res) => {
+       if (req.user) {
+               res.render((path.join(__dirname + '/../views/home.html')), { username: req.user.username });
+       }
+       else {
+              res.status(400).send({ message: 'User is not signed in' });
+       }
 }
--- config/env/development.js
+++ config/env/development.js
@@ -3,5 +3,11 @@ module.exports = {
        // or use 
        mongoUri: 'mongodb://admin:password@localhost:27017/testDB1',
        debug: true,
-       sessionSecret: 'dev_secret_key'
+       sessionSecret: 'dev_secret_key',
+        google: {
+              clientID: '156939899822-gca0h1n9cmpi6hr06e5knuf2q3fptaah.apps.googleusercontent.com',
+              clientSecret: 'kKiitGUN4tc6j5-kCZ63z9L4',
+              callbackURL: 'http://localhost:3000/oauth/google/callback'
+       }
+
 }
